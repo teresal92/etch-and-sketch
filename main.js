31909abcf5a26dd358b227bbcffd9569e7a03d1c
Teresa Lew <teresalew92@gmail.com>
2026-02-13 20:17:26 -0800

    render 16x16 grid

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello world!");
+const GRID_COUNT = 16;
+
+function createGrid() {
+  const container = document.querySelector(".container");
+
+  for (let i = 0; i < GRID_COUNT; i++) {
+    for (let j = 0; j < GRID_COUNT; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      container.appendChild(square);
+    }
+  }
+}
+
+createGrid();
